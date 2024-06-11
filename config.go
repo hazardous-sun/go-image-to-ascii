34f1a3b0f27ecb6f6acafbe9510ea25a1956bdd6
solaire <contact@unharmfulhazard.com>
2024-06-11 11:11:24 -0300

    renamed image_to_ascii.go to config.go

--- config.go
+++ config.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/png"
 	"os"
 	"strconv"
 	"strings"
@@ -108,43 +106,4 @@ func getResizeFactor(value string) (float64, error) {
 	}
 
 	return factor, nil
-}
-
-func readImage(imagePath string) {
-	// Open the image file
-	file, err := os.Open(imagePath)
-
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-
-	defer file.Close()
-
-	// Register PNG decoder
-	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
-
-	// Decode the image
-	img, err := png.Decode(file)
-	if err != nil {
-		fmt.Println("Error decoding image:", err)
-		return
-	}
-
-	// Get image bounds
-	bounds := img.Bounds()
-
-	// Loop through pixels and access RGB values
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			r, g, b, _ := img.At(x, y).RGBA()
-
-			// Convert to uint8 (optional)
-			red := uint8(r >> 8)
-			green := uint8(g >> 8)
-			blue := uint8(b >> 8)
-
-			fmt.Printf("Pixel (%d, %d): R: %d, G: %d, B: %d\n", x, y, red, green, blue)
-		}
-	}
 }
