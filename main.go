015489e0855dacdaac18f113ecd1a7580d9df4fa
solaire <contact@unharmfulhazard.com>
2024-06-11 09:58:24 -0300

    added a brief test for the build function

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
-package go_image_to_ascii
+package main
+
+import (
+	"os"
+)
 
 func main() {
+	args := os.Args
 
+	if len(args) < 2 {
+		printUsage("not enough args passed")
+	}
+
+	_, err := build(args[1:])
+
+	if err != nil {
+		println(err.Error())
+	}
 }
