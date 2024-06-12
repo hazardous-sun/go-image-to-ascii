a1e4df52046fe9ee642044ac9d8939d564556958
solaire <contact@unharmfulhazard.com>
2024-06-12 11:57:40 -0300

    started to add the logic to check the file format

--- image_parser.go
+++ image_parser.go
@@ -8,9 +8,16 @@ import (
 	"os"
 )
 
+type ImageData struct {
+	img    image.Image
+	height int
+	width  int
+	format string
+}
+
 func imageToAscii(config Config) {
 	// 1- open image
-	file, err := os.Open(imagePath)
+	file, err := os.Open(config.path)
 
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -23,6 +30,72 @@ func imageToAscii(config Config) {
 
 	// 3- resize image
 	// 4- turn the image bw
+}
+
+/*
+Tries to convert the file to Image and returns some metadata about it.
+Will fail if the file is an unsupported format.
+*/
+func convertImage(file os.File) (ImageData, error) {
+	var img image.Image
+	var height, width int
+	var format string
+
+	return ImageData{
+		img:    img,
+		height: height,
+		width:  width,
+		format: format,
+	}, nil
+}
+
+func getFormat(file *os.File) (*image.Image, error) {
+	if isJPG(file) {
+		return nil, nil
+	} else if isPNG(file) {
+		return nil, nil
+	} else if isGIF(file) {
+		return nil, nil
+	} else if isWEBP(file) {
+		return nil, nil
+	} else {
+		return nil, fmt.Errorf("format not supported")
+	}
+}
+
+func isJPG(file *os.File) bool {
+	buffer := make([]byte, 2)
+	_, err := file.ReadAt(buffer, 0)
+
+	if err != nil {
+		return false
+	} else if buffer[0] == 0xff && buffer[1] == 0xd8 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isPNG(file *os.File) bool {
+	buffer := make([]byte, 8)
+	_, err := file.ReadAt(buffer, 0)
+
+	if err != nil {
+		return false
+	} else if buffer[0] == 0x89 && buffer[1] == 'P' && buffer[2] == 'N' && buffer[3] == 'G' &&
+		buffer[4] == '\r' && buffer[5] == '\n' && buffer[6] == 0x1a && buffer[7] == '\n' {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isGIF(file *os.File) bool {
+	return false
+}
+
+func isWEBP(file *os.File) bool {
+	return false
 }
 
 func resizeImage(img image.Image, factor float64) image.Image {
